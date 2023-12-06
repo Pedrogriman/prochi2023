0dda7dda72e100435982c426b9041f82168c0b5e
Pedrogriman <132269873+Pedrogriman@users.noreply.github.com>
2023-12-06 10:16:56 -0400

    Update librerias_datos.py

--- librerias_datos.py
+++ librerias_datos.py
@@ -21,5 +21,12 @@ def leer_datos(ruta):
 
 # Limpieza de datos
 def limpiar_datos(datos):
-    # Colocar aquí el código de limpieza
+    datos.drop(['Unnamed: 0.3', 'Unnamed: 0.2',	'Unnamed: 0.1',	'Estatus',	'Unnamed: 0', 'Region',	'Barrio',	'Vendedor', 'Tipo', 'Moneda', 'Moneda Gastos',  
+            'Ambientes', 'Amoblado', 'Antigüedad', 'Área de cine', 'Área de juegos infantiles', 'Azotea', 'Balcón', 'Baño de visitas', 'Bodegas', 'Business center', 
+            'Caldera', 'Cancha de básquetbol', 'Cancha de paddle', 'Cancha de tenis', 'Canchas de usos múltiples', 'Cantidad de pisos', 'Cantidad máxima de habitantes', 
+            'Chimenea', 'Cisterna', 'Closets', 'Cocina', 'Comedor', 'Con cancha de fútbol', 'Con conexión para lavarropas', 'Con energia solar', 'Con TV satelital', 
+            'Departamentos por piso', 'Desayunador', 'Disponible desde', 'Dormitorio en suite', 'Dormitorio y baño de servicio', 'Estacionamiento de visitas', 'Gastos comunes', 
+            'Generador eléctrico', 'Gimnasio', 'Homeoffice', 'Jardín', 'Lavandería', 'Línea telefónica', 'Living', 'Logia', 'Número de piso de la unidad', 'Número de torre', 
+            'Orientación', 'Patio', 'Piscina', 'Playroom', 'Quincho', 'Recepción', 'Refrigerador', 'Salón de fiestas', 'Salón de usos múltiples', 'Sauna', 'Superficie de terraza',
+            'Terraza', 'Tipo de departamento', 'Uso comercial', 'Walk-in clóset', 'Jacuzzi', 'Tipo de seguridad', 'Con condominio cerrado', 'Mercado'],axis=1, inplace=True)
     pass
